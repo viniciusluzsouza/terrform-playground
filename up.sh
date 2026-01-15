4dccb6af88d10bcb55609aceb5c5fd1448c9b28a
viniciusluzouza <vinicius.souza@getambush.com>
2026-01-15 15:33:20 -0300

    refactor: move requirements.txt to lambda folder

--- up.sh
+++ up.sh
@@ -2,7 +2,7 @@
 
 # Build
 mkdir lambda/package
-pip3 install -r requirements.txt --target lambda/package
+pip3 install -r lambda/requirements.txt --target lambda/package
 cp lambda/lambda.py lambda/package/
 
 # Create the resources
